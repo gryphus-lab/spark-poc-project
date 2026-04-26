cca14e42f01326e43b06e5fc9b7bb5e8b2ae00fb
gryphus-lab <80040733+gryphus-lab@users.noreply.github.com>
2026-04-26 21:10:08 +0200

    add expecred schema and quality tests

--- src/transformations.py
+++ src/transformations.py
@@ -1,5 +1,15 @@
 from pyspark.sql.functions import col, lower
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType
 
 
 def clean_text_data(df, column_name):
     return df.withColumn(column_name, lower(col(column_name)))
+
+
+EXPECTED_SCHEMA = StructType(
+    [
+        StructField("id", IntegerType(), nullable=False),
+        StructField("name", StringType(), nullable=True),
+        StructField("status", StringType(), nullable=True),
+    ]
+)
